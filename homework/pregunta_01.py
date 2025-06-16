d72ce4216a9b6ddf81402c08855e039aa84ca49b
danlondonova <danlondonova@unal.edu.co>
2025-06-15 20:28:33 -0500

    respuesta

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,10 +2,151 @@
 """
 Escriba el codigo que ejecute la accion solicitada.
 """
-
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
 
 def pregunta_01():
-    """
+    output_dir = "docs"
+    os.makedirs(output_dir, exist_ok=True)
+
+    df = pd.read_csv("files/input/shipping-data.csv")
+    return df
+
+def create_visual_for_shipping_per_warehouse(df):
+    df = df.copy()
+    plt.figure()
+    counts = df.Warehouse_block.value_counts()
+    counts.plot.bar(
+        title="Shipping per Warehouse",
+        xlabel="Warehouse block",
+        ylabel="Record Count",
+        color="tab:blue",
+        fontsize=8,
+    )
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    output_path = "docs/shipping_per_warehouse.png"
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
+
+def create_visual_for_mode_of_shipment(df):
+    df = df.copy()
+    plt.figure()
+    counts = df.Mode_of_Shipment.value_counts()
+    counts.plot.pie(
+        title="Mode of shipment",
+        wedgeprops=dict(width=0.35),
+        ylabel="",
+        colors=["tab:blue", "tab:orange", "tab:green"],
+    )
+    output_path = "docs/mode_of_shipment.png"
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
+
+    
+def create_visual_for_average_customer_rating(df):
+    df = df.copy()
+    plt.figure()
+    df = (
+        df[["Mode_of_Shipment", "Customer_rating"]]
+        .groupby("Mode_of_Shipment")
+        .describe()
+    )
+    df.columns = df.columns.droplevel()
+    df = df[["mean", "min", "max"]]
+
+    plt.barh(
+        y=df.index.values,
+        width=df["max"].values - 1,
+        left=df["min"].values,
+        height=0.9,
+        color="lightgray",
+        alpha=0.8,
+    )
+    colors = [
+    "tab:green" if value >= 3.0 else "tab:orange" for value in df["mean"].values
+     ]
+
+    plt.barh(
+    y=df.index.values,
+    width=df["mean"].values - 1,
+    left=df["min"].values,
+    color=colors,
+    height=0.5,
+    alpha=1.0,
+    )
+    plt.title("Average Customer Rating")
+    plt.gca().spines["left"].set_color("gray")
+    plt.gca().spines["bottom"].set_color("gray")
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+
+    output_path = "docs/average_customer_rating.png"
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
+
+def create_visual_for_weight_distribution(df):
+    df = df.copy()
+    plt.figure()
+    df.Weight_in_gms.plot.hist(
+        title="Shipped Weight Distribution",
+        color="tab:orange",
+        edgecolor="white",
+    )
+
+    plt.gca().spines["top"].set_visible(False)
+    plt.gca().spines["right"].set_visible(False)
+    output_path = "docs/weight_distribution.png"
+    plt.tight_layout()
+    plt.savefig(output_path)
+    plt.close()
+
+
+    
+    html_content = """<!DOCTYPE html>
+<html>
+  <body>
+    <h1>Shipping Dashboard Example</h1>
+
+    <div style="width:45%;float:left">
+      <img src="shipping_per_warehouse.png" alt="Fig 1">
+      <img src="mode_of_shipment.png" alt="Fig 2">
+    </div>
+
+    <div style="width:45%;float:left">
+      <img src="average_customer_rating.png" alt="Fig 3">
+      <img src="weight_distribution.png" alt="Fig 4">
+    </div>
+  </body>
+</html>
+"""
+
+    html_path = "docs/index.html"
+    with open(html_path, "w", encoding="utf-8") as f:
+        f.write(html_content)
+
+    return html_path
+
+
+df = pregunta_01()
+create_visual_for_shipping_per_warehouse(df)
+create_visual_for_mode_of_shipment(df)
+create_visual_for_average_customer_rating(df)
+create_visual_for_weight_distribution(df)
+
+html_file_path = create_visual_for_weight_distribution(df)
+
+html_file_path
+
+
+
+"""
     El archivo `files//shipping-data.csv` contiene información sobre los envios
     de productos de una empresa. Cree un dashboard estático en HTML que
     permita visualizar los siguientes campos:
